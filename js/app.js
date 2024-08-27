58123bd72f9a70ef8e9d347d3241c9cf46ff4204
sunnercom <nuriddinovsunnatbek9@gmail.comm>
2024-08-27 09:29:17 +0500

    media is not finish

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const navLink = document.querySelector('.nav__link')
 const burger = document.querySelector('.burger')
-
+const deactive = document.querySelector('deactive')
 function burgerActive() {
     navLink.classList.toggle("burger__active")
 }
+function active() {
+    deactive.classList.toggle("active")
+}
 
-burger.addEventListener(("click")  , burgerActive)
+burger.addEventListener(("click")  , burgerActive , active)
